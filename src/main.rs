5078c81ed6b39eda0f9ba82793a222aa3b42c7ca
Gachara <bgachara@gmail.com>
2023-04-15 09:54:10 +0300

    flush function implementation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::env::args;
-use std::fs::write;
-use std::iter::Skip;
 use std::collections::HashMap;
+use std::env::args;
+//use std::fs::write;
+use std::iter::Skip;
 
 fn main() {
     let mut arguments: Skip<std::env::Args> = args().skip(1);
@@ -9,38 +9,52 @@ fn main() {
     let value: String = arguments.next().unwrap();
     println!("The key is '{}' and the value is '{}'", key, value);
 
-    let contents: String = format!("{}\t{}\n", key, value);
-    write("jara.db", contents);
+    //let contents: String = format!("{}\t{}\n", key, value);
+    //write("jara.db", contents);
 
-    let database: Database = Database::new();
-    
+    let mut database: Database = Database::new().expect("What is this");
+    database.insert(key.to_uppercase(), value.clone());
+    database.insert(key, value);
+    database.flush().unwrap();
 }
 
-
 struct Database {
-    map: Hashmap<String, String>,
+    map: HashMap<String, String>,
 }
 
 impl Database {
     fn new() -> Result<Database, std::io::Error> {
         //read the jara db file
-        let mut map = Hashmap::new();
-        let contents: String = match std::fs::read_to_string("jara.db") {
-            Ok(c:String) => c,
-            Err(error:Error) => {
-                return Err(error);
-            }
-        };
         //other implementation of above
-        //let contents: String = std::fs::read_to_string("jara.db")?;
-        for line: &str in contents.lines(){
+        let mut map = HashMap::new();
+        //let contents: String = match std::fs::read_to_string("jara.db") {
+        //Ok(c:String) => c;
+        //Err(error:Error) => {
+        //return Err(error);
+        //}
+        //};
+        let contents: String = std::fs::read_to_string("jara.db")?;
+        for line in contents.lines() {
             let mut chunks = line.splitn(2, '\t');
             let key: &str = chunks.next().expect("No key");
             let value: &str = chunks.next().expect("No value");
-            map.insert(key, value);
+            map.insert(key.to_owned(), value.to_owned());
         }
         //parse the string
         //populate our map
-        Ok(Database{map: map})
+        Ok(Database { map: map })
+    }
+
+    fn insert(&mut self, key: String, value: String) {
+        self.map.insert(key, value);
+    }
+
+    fn flush(self) -> std::io::Result<()> {
+        let mut contents = String::new();
+        for pairs in self.map {
+            let kvpair = format!("{}\t,{}\n", pairs.0, pairs.1);
+            contents.push_str(&kvpair);
+        }
+        std::fs::write("kv.db", contents)
     }
 }
